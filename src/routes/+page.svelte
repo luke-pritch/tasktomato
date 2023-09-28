06bab17273e09a15f1f16349b27af5a70652ccf9
Luke Pritchard <lp@lukepritchard.ca>
2023-09-27 21:32:48 -0400

    alert

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,58 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
 	import timer, { startTimer, resetTimer } from './timer';
 
 	let minutes: number;
 	let seconds: number;
 	let isActive: boolean;
+	let timerDone: boolean = false;
+
+	let alertSound: HTMLAudioElement; // Reference to the audio element
+
+	// Function to play the alert sound
+	function playAlertSound() {
+		alertSound.play();
+	}
+
+	// Function to display a browser notification
+	function displayNotification() {
+		if (!('Notification' in window)) {
+			console.error('This browser does not support desktop notification');
+			return;
+		}
+
+		if (Notification.permission === 'granted') {
+			new Notification('Pomodoro Timer', { body: 'Time is up!' });
+		} else if (Notification.permission !== 'denied') {
+			Notification.requestPermission().then((permission) => {
+				if (permission === 'granted') {
+					new Notification('Pomodoro Timer', { body: 'Time is up!' });
+				}
+			});
+		}
+	}
 
 	const unsubscribe = timer.subscribe(($timer) => {
 		minutes = $timer.minutes;
 		seconds = $timer.seconds;
 		isActive = $timer.isActive;
+
+		if (minutes === 0 && seconds === 0 && isActive) {
+			timerDone = true;
+			isActive = false; // Set isActive to false when the timer hits zero
+			playAlertSound();
+			displayNotification();
+		}
+	});
+
+	onMount(() => {
+		if (Notification.permission !== 'granted' && Notification.permission !== 'denied') {
+			Notification.requestPermission();
+		}
+	});
+
+	onDestroy(() => {
+		unsubscribe();
 	});
 </script>
 
@@ -19,23 +63,29 @@
 
 <section class="flex flex-col justify-center items-center flex-grow">
 	<h1 class="text-3xl font-semibold w-full text-center">Pomodoro Timer</h1>
-	<div class="text-2xl my-4">
+	<div class={timerDone ? 'text-2xl my-4 text-red-500' : 'text-2xl my-4'}>
 		{minutes}:{seconds < 10 ? '0' : ''}{seconds}
 	</div>
 	<button
-		on:click={() => startTimer()}
+		on:click={() => {
+			startTimer();
+		}}
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2"
 	>
-		{isActive ? 'Pause' : 'Start'}
+		{isActive && !timerDone ? 'Pause' : 'Start'}
 	</button>
 	<button
-		on:click={() => resetTimer()}
+		on:click={() => {
+			resetTimer();
+			timerDone = false;
+		}}
 		class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
 	>
 		Reset
 	</button>
+	{#if timerDone}
+		<div class="mt-4 bg-yellow-500 p-2 rounded">Time's up! Take a break.</div>
+	{/if}
+	<audio src="/alarm.mp3" bind:this={alertSound} />
+	<!-- Binding the audio element -->
 </section>
-
-<style>
-	/* You can keep the styles empty if you are fully using Tailwind CSS */
-</style>
